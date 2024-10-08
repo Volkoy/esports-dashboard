b8d7378a2781e0ca4ffeb727031fde598931fcd1
Henrique Vaz <henrique.m.vaz@tecnico.ulisboa.pt>
2024-10-08 10:41:30 +0100

    fix ghost selected lines bug

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var selectedGenres;
 var years;
 var selectedLines = [];
 var colorScheme = {
-  "Strategy": "#a7cde3",
+  Strategy: "#a7cde3",
   "First-Person Shooter": "#2b77b3",
-  "Sports": "#afdd8f",
+  Sports: "#afdd8f",
   "Fighting Game": "#299d39",
-  "Racing": "#fb9f97",
+  Racing: "#fb9f97",
   "Multiplayer Online Battle Arena": "#e23703",
   "Role-Playing Game": "#fbc171",
   "Third-Person Shooter": "#fe8600",
@@ -17,7 +17,6 @@ var colorScheme = {
   "Puzzle Game": "#fcfe9e",
   "Battle Royale": "#b05e26",
 };
-
 
 var minRange, maxRange;
 var isAdjusted = false;
@@ -32,7 +31,6 @@ function yearFilter() {
 
   let minYear = 1998; // Set the starting year
   let maxYear = 2023; // Set the ending year
-  
 
   const minRangeFill = () => {
     // Calculate the left position based on the current year range
@@ -66,7 +64,9 @@ function yearFilter() {
       setMaxValueOutput();
       minRangeFill();
       maxRangeFill();
-      filtered = globalData.filter((d)=> d.Year >= minRange && d.Year <= maxRange)
+      filtered = globalData.filter(
+        (d) => d.Year >= minRange && d.Year <= maxRange
+      );
       years = [...new Set(filtered.map((d) => d.Year))];
       updateLineChart(switchEarningData(filtered));
       if (maxRange - minRange < minRangeValueGap) {
@@ -90,21 +90,23 @@ function yearFilter() {
 function init() {
   d3.json("esports.json").then(function (data) {
     globalData = data;
-    yearFilter()
+    yearFilter();
     createLineChart(globalData);
   });
 }
 
-function switchEarningData(data)  {
+function switchEarningData(data) {
   let aggregatedData;
   if (isAdjusted) {
+    selectedLines = [];
     aggregatedData = d3.rollup(
       data,
       (v) => d3.sum(v, (d) => d.AdjustedEarnings),
       (d) => d.Genre,
       (d) => d.Year
     );
-  }else {
+  } else {
+    selectedLines = [];
     aggregatedData = d3.rollup(
       data,
       (v) => d3.sum(v, (d) => d.Earnings),
@@ -145,7 +147,6 @@ function createLineChart(data) {
     .style("pointer-events", "none")
     .style("opacity", 0);
 
-  
   const formattedData = switchEarningData(data, false);
   years = [...new Set(data.map((d) => d.Year))];
   genres = [...new Set(data.map((d) => d.Genre))];
@@ -207,10 +208,9 @@ function createLineChart(data) {
         .style("background-color", "lightgrey")
         .style("color", "black")
         .property("disabled", false);
-        let updatedData = switchEarningData(data);
-        updatedData = updatedData.filter((g) =>
-          selectedGenres.includes(g.genre));
-        updateLineChart(updatedData)
+      let updatedData = switchEarningData(data);
+      updatedData = updatedData.filter((g) => selectedGenres.includes(g.genre));
+      updateLineChart(updatedData);
     });
   d3.select(".linechart-buttons")
     .append("button")
@@ -231,10 +231,9 @@ function createLineChart(data) {
         .style("background-color", "lightgrey")
         .style("color", "black")
         .property("disabled", false);
-      let updatedData = switchEarningData(data)
-      updatedData = updatedData.filter((g) =>
-        selectedGenres.includes(g.genre));
-      updateLineChart(updatedData)
+      let updatedData = switchEarningData(data);
+      updatedData = updatedData.filter((g) => selectedGenres.includes(g.genre));
+      updateLineChart(updatedData);
     });
 
   const svg = d3
@@ -374,25 +373,31 @@ function createLineChart(data) {
         const isSelected = selectedLines.includes(genreData.genre);
 
         if (isSelected) {
-            // If the line is already selected, remove it from the array
-            selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
+          // If the line is already selected, remove it from the array
+          selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
         } else {
-            // If not, add it to the selected array
-            selectedLines.push(genreData.genre);
+          // If not, add it to the selected array
+          selectedLines.push(genreData.genre);
         }
 
         // Set opacity for all lines
         svg.selectAll(".line").each(function () {
-            const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
-            d3.select(this).style("opacity", selectedLines.includes(lineGenre) ? 1 : 0.2);
+          const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(lineGenre) ? 1 : 0.2
+          );
         });
 
         // Set opacity for all circles
         svg.selectAll(".circle").each(function () {
-            const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
-            d3.select(this).style("opacity", selectedLines.includes(circleGenre) ? 1 : 0.2);
+          const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(circleGenre) ? 1 : 0.2
+          );
         });
-    });
+      });
 
     svg
       .selectAll(`.circle-${index}`)
@@ -436,29 +441,36 @@ function createLineChart(data) {
           .style("stroke", "none");
 
         tooltip.transition().duration(200).style("opacity", 0);
-      }).on("click", function () {
+      })
+      .on("click", function () {
         const isSelected = selectedLines.includes(genreData.genre);
 
         if (isSelected) {
-            // If the line is already selected, remove it from the array
-            selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
+          // If the line is already selected, remove it from the array
+          selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
         } else {
-            // If not, add it to the selected array
-            selectedLines.push(genreData.genre);
+          // If not, add it to the selected array
+          selectedLines.push(genreData.genre);
         }
 
         // Set opacity for all lines
         svg.selectAll(".line").each(function () {
-            const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
-            d3.select(this).style("opacity", selectedLines.includes(lineGenre) ? 1 : 0.2);
+          const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(lineGenre) ? 1 : 0.2
+          );
         });
 
         // Set opacity for all circles
         svg.selectAll(".circle").each(function () {
-            const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
-            d3.select(this).style("opacity", selectedLines.includes(circleGenre) ? 1 : 0.2);
+          const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(circleGenre) ? 1 : 0.2
+          );
         });
-    });;
+      });
   });
 
   // X and Y axes
@@ -513,7 +525,7 @@ function createLineChart(data) {
 
 function updateLineChart(data) {
   const container = d3.select(".LineChart");
-  console.log("update data")
+  console.log("update data");
   d3.select("svg").remove();
 
   const svgWidth = container.node().getBoundingClientRect().width;
@@ -550,18 +562,18 @@ function updateLineChart(data) {
 
   data.forEach((genreData, index) => {
     const genreColor = colorScheme[genreData.genre];
-  
+
     const tooltip = d3
-    .select(".LineChart")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("padding", "8px")
-    .style("background-color", "white")
-    .style("border", "1px solid #ccc")
-    .style("border-radius", "4px")
-    .style("pointer-events", "none")
-    .style("opacity", 0);
+      .select(".LineChart")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("padding", "8px")
+      .style("background-color", "white")
+      .style("border", "1px solid #ccc")
+      .style("border-radius", "4px")
+      .style("pointer-events", "none")
+      .style("opacity", 0);
 
     svg
       .append("path")
@@ -596,25 +608,31 @@ function updateLineChart(data) {
         const isSelected = selectedLines.includes(genreData.genre);
 
         if (isSelected) {
-            // If the line is already selected, remove it from the array
-            selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
+          // If the line is already selected, remove it from the array
+          selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
         } else {
-            // If not, add it to the selected array
-            selectedLines.push(genreData.genre);
+          // If not, add it to the selected array
+          selectedLines.push(genreData.genre);
         }
 
         // Set opacity for all lines
         svg.selectAll(".line").each(function () {
-            const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
-            d3.select(this).style("opacity", selectedLines.includes(lineGenre) ? 1 : 0.2);
+          const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(lineGenre) ? 1 : 0.2
+          );
         });
 
         // Set opacity for all circles
         svg.selectAll(".circle").each(function () {
-            const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
-            d3.select(this).style("opacity", selectedLines.includes(circleGenre) ? 1 : 0.2);
+          const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(circleGenre) ? 1 : 0.2
+          );
         });
-    });
+      });
 
     svg
       .selectAll(`.circle-${index}`)
@@ -658,29 +676,36 @@ function updateLineChart(data) {
           .style("stroke", "none");
 
         tooltip.transition().duration(200).style("opacity", 0);
-      }).on("click", function () {
+      })
+      .on("click", function () {
         const isSelected = selectedLines.includes(genreData.genre);
 
         if (isSelected) {
-            // If the line is already selected, remove it from the array
-            selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
+          // If the line is already selected, remove it from the array
+          selectedLines.splice(selectedLines.indexOf(genreData.genre), 1);
         } else {
-            // If not, add it to the selected array
-            selectedLines.push(genreData.genre);
+          // If not, add it to the selected array
+          selectedLines.push(genreData.genre);
         }
 
         // Set opacity for all lines
         svg.selectAll(".line").each(function () {
-            const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
-            d3.select(this).style("opacity", selectedLines.includes(lineGenre) ? 1 : 0.2);
+          const lineGenre = d3.select(this).attr("id"); // Get genre from line ID
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(lineGenre) ? 1 : 0.2
+          );
         });
 
         // Set opacity for all circles
         svg.selectAll(".circle").each(function () {
-            const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
-            d3.select(this).style("opacity", selectedLines.includes(circleGenre) ? 1 : 0.2);
+          const circleGenre = d3.select(this).attr("id"); // Assuming circles have IDs matching their genres
+          d3.select(this).style(
+            "opacity",
+            selectedLines.includes(circleGenre) ? 1 : 0.2
+          );
         });
-    });;
+      });
   });
 
   // X and Y axes
